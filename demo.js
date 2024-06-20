efa30b245491db05ffb8649e01673b3c7db35328
Naveen <naveen638340@gmail.com>
2024-06-20 21:36:47 +0530

    20.06.24

---
+++ demo.js
@@ -0,0 +1,65 @@
+// let a=10
+// let b=10
+// let c=a+b
+// document.write("The value is ",c)
+// document.write("<br>")
+// let age=19
+// if(age<=18){
+//     document.write("Not eligibe to vote")
+
+// }
+// else{
+//     document.write("Eligible to vote")
+//     document.write("<br>")
+// }
+
+// let str="hello"
+// //d=str.slice(6,10)
+// //document.write(str.length)
+// //document.write("<br>")
+// //document.write(d)
+// document.write("<br>")
+// document.write(str.substring(6,10))
+// document.write("<br>")
+// name()
+// function name(){
+//     var flower=["lilly","jasmine","lotus"]
+//     document.write(flower)
+
+
+// }document.write("<br>") 
+// let abc=()=>{
+//     var flower=["lilly","jasmine","lotus"]
+//     document.write(flower)
+// }
+// abc()
+// const pen={
+//     name:"Raynolds",
+//     price:25,
+//     color:"red",
+//     store:"Sagar"
+// }
+// console.log("The pen Name is",pen.name,"Price is",pen.price)
+// let str1="naveen"
+// let str2="kumar"
+// str1===str2 ?"yes":"no";
+// // if(str1===str2)
+// // {
+// //     console.log("equal")
+
+// // }
+// // else{
+// //     console.log("not")
+// // }
+// // (str1===str2)?"yes":"no";
+let guess=10
+guess_no=prompt("Enter the number")
+
+if(guess==guess_no){
+    document.write("congrates")
+
+}
+else{
+    console.log("wrong")
+    guess_no=prompt("Enter the number")
+}
